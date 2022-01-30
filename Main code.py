a825e1d47b065b8c5f5cb874d1b57d651d3ac877
General-Iroh1 <97752135+General-Iroh1@users.noreply.github.com>
2022-01-30 18:13:41 +0000

    Create Main code.py

---
+++ Main code.py
@@ -0,0 +1,20 @@
+UserWordInput = []
+i = 0
+while i < 3:
+    word = input('Enter A Word you want your password te be a mixture of(The maximum is 3): ')
+    UserWordInput.append(word)
+
+    choice = input('Would you like to enter another word? Press y if you do, and n if you dont: ')
+    if choice.casefold() == 'n':
+        break
+    if choice.casefold() == 'y':
+        i += 1
+    else:
+        print ("Sorry, you have typed a wrong input.")
+        break
+
+
+for element in UserWordInput:
+        print(element)
+
+
