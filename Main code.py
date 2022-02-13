9bc20359f6b9d14c0aa645dceddac5ee6e7c027d
General-Iroh1 <97752135+General-Iroh1@users.noreply.github.com>
2022-02-13 09:55:23 +0000

    added password saver, added 3 retrys

--- Main code.py
+++ Main code.py
@@ -5,19 +5,41 @@ UserWordInput = []
 i = 0
 n = 0
 while True:
-    word = input("Enter A Word you want your password te be a mixture of(The maximum is 3): ")
+    word = input("Enter A Word you want your password te be a mixture of(The maximum is 3)\n")
     UserWordInput.append(word); n += 1
     if n == 3:
         break
 
-    choice = input("Would you like to enter another word? Type 'yes' if you do, and 'no' if you dont: ")
+    choice = input("Would you like to enter another word? Type 'yes' if you do, and 'no' if you dont\n")
 
     if choice.casefold() == 'no':
         break
     if choice.casefold() == 'yes':
         continue
     else:
-        print ("Sorry, you have typed a wrong input, your input has been counted as 'yes'")
+        print ("Sorry, you have typed a wrong input, which would you like? No or Yes?")
+        wronginput = input("Input either 'yes' or 'no'\n")
+        if wronginput == "yes":
+            continue
+        if wronginput == "no":
+            break
+        else:
+            print("You have misspelled either 'yes' or 'no', please try again")
+            wronginput2 = input("yes or no?")
+            if wronginput2 == "no":
+                break
+            if wronginput2 == "yes":
+                continue
+            else:
+                print("You have one more try until you have to restart the program, please enter either yes or no very carefully.⚠️ IF YOU MISSPELL AGAIN, YOU WILL NOT GET ANOTHER TRY!\n")
+                wronginput3 = input("")
+                if wronginput3 == "yes":
+                    continue
+                if wronginput3 == "no":
+                    break
+                else:
+                    print("You have misspelled 3 times, please restart the program.")
+
     continue
     
 
@@ -29,7 +51,7 @@ VariableForList = VariableForList.replace('i', "!")
 VariableForList = VariableForList.replace('a', "@")
 VariableForList = VariableForList.replace('o', "0")
 VariableForList = VariableForList.replace('q', "9")
-VariableForList = VariableForList.replace('e', "£")
+VariableForList = VariableForList.replace('e', "E")
 VariableForList = VariableForList.replace('u', "U")
 
 #joining numbers to end and start of password
@@ -37,7 +59,10 @@ print ("The password you have created is:", VariableForList)
 
 
 
-stdoutOrigin=sys.stdout 
-sys.stdout = open("Password Saver.txt", "w")
-sys.stdout.close()
-sys.stdout=stdoutOrigin
+reason = input("Why are you using this password?\n")
+sys.stdout = open("Password Saver.txt", "a")
+sys.stdout.write ("\n")
+sys.stdout.write (reason)
+sys.stdout.write (" password is: ")
+sys.stdout.write (VariableForList)
+
