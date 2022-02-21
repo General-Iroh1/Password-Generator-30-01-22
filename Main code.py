925e42b5e8b6d5d2e36280988a21005dfdae6c97
General-Iroh1 <97752135+General-Iroh1@users.noreply.github.com>
2022-02-21 09:41:36 +0000

    added password checker

--- Main code.py
+++ Main code.py
@@ -1,13 +1,11 @@
 from __future__ import print_function
-
-
 import atexit
 from os import path
 from json import dumps, loads
 
 
 def read_counter():
-    return loads(open("counter.json", "r").read()) + 1 if path.exists("counter.json") else 0
+    return loads(open("counter.json", "r").read()) + 1 if path.exists("counter.json") else 1
 
 
 def write_counter():
@@ -18,12 +16,13 @@ def write_counter():
 counter = read_counter()
 atexit.register(write_counter)
 import sys
-from colorama import Fore
+from colorama import Fore, Back, Style
 #while loop
 UserWordInput = []
 i = 0
 n = 0
 while True:
+    print(Fore.WHITE,"")
     word = input("Enter A Word you want your password te be a mixture of(The maximum is 3)\n")
     UserWordInput.append(word); n += 1
     if n == 3:
@@ -57,7 +56,7 @@ while True:
                 if wronginput3 == "no":
                     break
                 else:
-                    print(Fore.BLACK,"You have misspelled 3 times, please restart the program to input more words.")
+                    print(Fore.LIGHTBLACK_EX,"You have misspelled 3 times, please restart the program to input more words.")
                     break
     
 
@@ -66,21 +65,36 @@ WordConnector = ""
 VariableForList=(WordConnector.join(UserWordInput))
 #Replacing words with symbols/numbers
 VariableForList = VariableForList.replace('i', "!")
+VariableForList = VariableForList.replace('I', "!")
 VariableForList = VariableForList.replace('a', "@")
+VariableForList = VariableForList.replace('A', "@")
 VariableForList = VariableForList.replace('o', "0")
+VariableForList = VariableForList.replace('O', "0")
+VariableForList = VariableForList.replace('Q', "9")
 VariableForList = VariableForList.replace('q', "9")
 VariableForList = VariableForList.replace('e', "E")
 VariableForList = VariableForList.replace('u', "U")
-
+VariableForList = VariableForList.replace('s', "$")
+VariableForList = VariableForList.replace('S', "$")
 #joining numbers to end and start of password
-print (Fore.LIGHTBLUE_EX,"The password you have created is:", VariableForList)
-
+print (Fore.LIGHTBLUE_EX,"The password you have created is:", VariableForList, "*Saved*")
 
 print(Fore.WHITE,"")
 reason = input("Why are you using this password?\n")
-sys.stdout = open("Password Saver.txt", "a")
+sys.stdout = open("Password Saver.rtf", "a")
+sys.stdout.write (Back.LIGHTBLUE_EX)
 sys.stdout.write (reason)
 sys.stdout.write (" password is: ")
 sys.stdout.write (VariableForList)
-sys.stdout.write (" You have ran the code {} times/time".format(counter))
-sys.stdout.write ("\n")
+sys.stdout.write ("\n\tYou have ran the code {} times/time\n".format(counter))
+
+count = len(VariableForList)
+if count == 1 or count == 2 or count == 3 or count == 4 or count == 0:
+    print("\tThis password is - Weak")
+    print("\tThe length of this password is: ", count, "characters long\n")
+if count == 5 or count == 6 or count == 7 or count == 8 or count == 9:
+    print("\tThis password is - Average")
+    print("\tThe length of this password is: ", count, "characters long\n")
+if count == 10 or count == 11 or count == 12 or count == 13 or count == 15 or count == 16 or count == 17 or count == 18 or count == 19 or count == 20:
+    print("\tThis password is - Strong")
+    print("\tThe length of this password is: ", count, "characters long\n")
