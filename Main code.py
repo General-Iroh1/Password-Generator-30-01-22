c4c31f0c92552aca09ad4a09926ac7af17513c46
General-Iroh1 <97752135+General-Iroh1@users.noreply.github.com>
2022-02-24 17:26:41 +0000

    attemp to color reason

--- Main code.py
+++ Main code.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 import atexit
 from os import path
 from json import dumps, loads
+from simple_colors import *
+from termcolor import colored
 
 
 def read_counter():
@@ -81,8 +83,9 @@ print (Fore.LIGHTBLUE_EX,"The password you have created is:", VariableForList, "
 
 print (Fore.WHITE)
 reason = input("Why are you using this password?\n")
-sys.stdout = open("Password Saver.rtf", "a")
-sys.stdout.write ("\\rtf1 ")
+reason = colored(reason)
+sys.stdout = open("Password Saver.txt", "a")
+print (reason)
 sys.stdout.write (" password is: ")
 sys.stdout.write (VariableForList)
 sys.stdout.write ("\n\tYou have ran the code {} times/time\n".format(counter))
