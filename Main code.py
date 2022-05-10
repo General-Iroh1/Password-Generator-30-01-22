ddafac2e82602daf63ee1c16b13e47299af6cfa9
General-Iroh1 <97752135+General-Iroh1@users.noreply.github.com>
2022-05-10 18:07:34 +0100

    trying to recreate other programm

--- Main code.py
+++ Main code.py
@@ -2,11 +2,11 @@ reason = input("Why would you like to use this password?\n")
 f = open("Password Saver.rtf", "a+")
 f = open("Password Saver.rtf", "r+")
 occ = 0
-CPStorage = input("Would you like to clear the password storage?\n")
-if CPStorage == "yes".casefold():
-    f.truncate(0)    
-    f.write("Cleared!")
-
+for line in f:
+    for word in f:
+        if word == reason:
+            occ += 1
+        occ += 1
 from os import path
 from json import dumps, loads
 def read_counter():
