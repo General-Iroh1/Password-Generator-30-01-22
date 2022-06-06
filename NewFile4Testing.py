b5e8a9482dfe888618668477d122ebe1bcca15ab
General-Iroh1 <97752135+General-Iroh1@users.noreply.github.com>
2022-06-06 18:25:01 +0100

    Testing Overflow solutions.

--- NewFile4Testing.py
+++ NewFile4Testing.py
@@ -1,33 +1,4 @@
-with open("NewFile4Testing.rtf", "r+") as f:
-        for i in f:
-            #importing Path class.
-            from pathlib import Path
-    
-  
-    #
-    #
-            def replacetext(search_text, replace_text):
-  
-    # 
-                file = Path("NewFile4Testing.rtf")
-  
-    # 
-    # 
-                data = file.read_text()
-  
-    # 
-                data = data.replace(search_text, replace_text)
-    # 
-    # 
-                file.write_text(data)
-  
-    # 
-        # 
-        # 
-                search_text = "python"
-        #
-        #
-                replace_text = "\n"
-#
-#
-                print(replacetext(search_text, replace_text))
+with open("NewFile4Testing.txt", "rt") as fin:
+    with open("out.txt", "wt") as fout:
+        for line in fin:
+            fout.write(line.replace("python", "\n"))
